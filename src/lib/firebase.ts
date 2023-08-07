fa8373f387f0e61abdbc495e238d07901b9a3353
Lucas Corey <lac2559@g.rit.edu>
2023-08-06 22:11:33 -0400

    firebase dev env test

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,25 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//     apiKey: process.env.FIREBASE_API_KEY,
+//     authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//     projectId: process.env.FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.FIREBASE_APP_ID,
+// };
+
+// // Initialize Firebase
+// export const app = initializeApp(firebaseConfig);
+
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.FIREBASE_APP_ID,
+    apiKey: "AIzaSyDmu7Nyn2QwPm0-z2W8p3uSWZyU4bAA1wU",
+    authDomain: "changelingvr-web.firebaseapp.com",
+    projectId: "changelingvr-web",
+    storageBucket: "changelingvr-web.appspot.com",
+    messagingSenderId: "296491725010",
+    appId: "1:296491725010:web:0312e0677ac3514f1efd0b",
+    measurementId: "G-QTK10Z84J1",
 };
 
 // Initialize Firebase
