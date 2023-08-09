d2c60db5d6bf9ba7a32edb3631c71ef28caa8f28
Lucas Corey <lac2559@g.rit.edu>
2023-08-09 15:27:49 -0400

    please work

--- src/lib/server/admin.ts
+++ src/lib/server/admin.ts
@@ -2,12 +2,20 @@ import { getAuth } from "firebase-admin/auth";
 import { getFirestore } from "firebase-admin/firestore";
 import pkg from "firebase-admin";
 
+import {
+    FIREBASE_PROJECT_ID,
+    FIREBASE_CLIENT_EMAIL,
+    FIREBASE_PRIVATE_KEY,
+} from "$env/static/private";
+
 try {
     pkg.initializeApp({
         credential: pkg.credential.cert({
-            projectId: process.env.FIREBASE_PROJECT_ID,
-            clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-            privateKey: process.env.FIREBASE_PRIVATE_KEY,
+            projectId: process.env.FIREBASE_PROJECT_ID ?? FIREBASE_PROJECT_ID,
+            clientEmail:
+                process.env.FIREBASE_CLIENT_EMAIL ?? FIREBASE_CLIENT_EMAIL,
+            privateKey:
+                process.env.FIREBASE_PRIVATE_KEY ?? FIREBASE_PRIVATE_KEY,
         }),
     });
 } catch (err: any) {
