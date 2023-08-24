bddd9888189369008631850ec271d6bc4b49708b
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2023-08-24 17:52:20 -0400

    Content-full home page

--- src/lib/components/NavList.svelte
+++ src/lib/components/NavList.svelte
@@ -6,7 +6,6 @@
 
 <nav>
     <ul class="flex gap-8 items-center {flexDirection}">
-        <li><a href="/about" aria-current={$page.url.pathname === "/about"}>About</a></li>
         <li><a href="/experiences" aria-current={$page.url.pathname === "/experiences"}>Experiences</a></li>
         <li><a href="/team" aria-current={$page.url.pathname === "/team"}>Team</a></li>
         <li><a href="https://changelingvrteam.itch.io/changelingvr" class="btn variant-filled-primary">Play Now</a></li>
