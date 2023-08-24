ad0d8473813a4d435e5bb02a48c24a97fe7c2496
Lucas Corey <89759400+ArgentumRhodon@users.noreply.github.com>
2023-08-24 11:03:23 -0400

    Added blurb

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,18 +2,21 @@
     import { onMount } from 'svelte';
     import type { PageData } from './$types';
     import {fade} from "svelte/transition"
+    import TypeWriter from '$lib/components/TypeWriter.svelte';
     
     export let data: PageData;
 
     let ready = false;
     onMount(() => ready = true)
+
+    const paragraph = "Changeling is a virtual reality Narrative Mystery game where sinister magics meet old-school detective work. Observe and enter the minds of your client's family in order to uncover the truth behind their child's sudden unresponsiveness.";
 </script>
 
 <svelte:head>
     <title>ChangelingVR | Home</title>
 </svelte:head>
 
-<section class="relative min-h-[31.25rem] md:p-[3.75rem]">
+<header class="relative min-h-[31.25rem] md:p-[3.75rem]">
     <div id="background-wrapper" class="hidden md:block absolute top-0 left-0 bottom-0 w-full bg-black">
         <div class="w-full h-full bg-black relative z-0">
             <video preload="auto" autoplay loop muted class="relative w-full h-full object-cover object-top">
@@ -33,4 +36,11 @@
             <img in:fade={{delay: 500, duration: 1000 }} src="logo.svg" alt="" class="w-[80vw] h-[35vw] md:w-[30vw] md:h-[17.63vw] z-10">
         {/if}
     </div>
-</section>
+</header>
+<main>
+    <div class="flex justify-center items-center bg-[url(/paper.png)] bg-no-repeat bg-cover min-h-[32.25rem]">
+        <div class="w-full flex flex-col max-w-[1000px]">
+            <TypeWriter text={paragraph}/>
+        </div>
+    </div>
+</main>
