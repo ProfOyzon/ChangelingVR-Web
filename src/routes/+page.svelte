05000a3c6a274471c5c40a3335b01f1a8cdad7d3
jf7689 <jf7689@rit.edu>
2024-01-30 15:53:26 -0500

    Set up html and styling for home page log in

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,64 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    export let form;
+</script>
+
+<div class="wrapper">
+    <form method="POST">
+        <p>{ form?.message || "" }</p>
+        
+        <div class="container">
+            <label for="email">Email</label>
+            <input type="text" id="email" name="email" placeholder="Email">
+            <div class="spacer-top">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" placeholder="Password">
+            </div>
+            <div class="spacer-top">
+                <button class="btn" type="submit">Log In</button>
+            </div>
+
+            <p>Already have an account? <a href="./signup">Sign up</a></p>
+        </div>
+    </form>
+</div>
+
+
+<style>
+    label{
+        display: block;
+        font-size: 1.25rem;
+    }
+
+    input {
+        width: 100%;
+        height: 1.5rem;
+    }
+
+    input:focus {
+        outline: none;
+    }
+
+    .wrapper {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 90vh;
+    }
+
+    .container {
+        padding: 1rem 2rem;
+        border-radius: .5rem;
+        border: solid lightgray 1px;
+    }
+
+    .spacer-top {
+        margin-top: 1rem;
+    }
+
+    .btn {
+        font-size: 1.25rem;
+        width: 100%;
+        padding: .25rem;
+    }
+</style>
