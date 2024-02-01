aea32ae63e570801598b210f352a4c07ccf19ef5
jf7689 <jf7689@rit.edu>
2024-02-01 14:48:32 -0500

    Able to send emails successfully

--- src/routes/reset/+page.server.js
+++ src/routes/reset/+page.server.js
@@ -13,12 +13,36 @@ export const actions = {
             };
         }
 
+        const text = `
+        Someone has requested a password reset.
+        
+        Use this link to reset your password:
+        https://changelingvr-web.vercel.app/
+
+        If this was a mistake, just ignore this email and nothing will happen.
+
+        Kind regards,
+        Changeling Web Team
+        `;
+
+        const html = `
+        <p>Someone has requested a password reset.</p?
+        
+        <p>Use this link to reset your password:<br>
+        <a target="_blank" href="https://changelingvr-web.vercel.app/">https://changelingvr-web.vercel.app/</a></p>
+
+        <p>If this was a mistake, just ignore this email and nothing will happen.</p>
+
+        <p>Kind regards,<br>
+        Changeling Web Team</p>
+        `;
+
         const message = {
             from: MAIL_EMAIL,
             to: email,
             subject: "Reset Your Password",
-            text: "",
-            html: ""
+            text: text,
+            html: html
         }
 
         const sendEmail = async (message) => {
